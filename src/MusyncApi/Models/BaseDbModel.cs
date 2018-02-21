40fed899a7654c6c1bd83ca47cf84c8fecb3fe5b
YÜCEL AKTAŞ <aktasyucel@hotmail.com>
2018-02-21 14:08:09 +0100

    TestModel and BaseDbModel are created.

---
+++ src/MusyncApi/Models/BaseDbModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusyncApi.Models
+{
+    public class BaseDbModel
+    {
+        string Id { get; set; }
+    }
+}
