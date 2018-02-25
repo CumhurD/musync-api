4f06d63c94309d54d769c9a6aeabb2a85b326a17
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-02-25 18:06:29 +0100

    applied dependency injection for TestRepository.

--- src/MusyncApi/Repository/TestRepository.cs
+++ src/MusyncApi/Repository/TestRepository.cs
@@ -3,44 +3,43 @@ using System.Linq;
 using musync.api.Repository.Interfaces;
 using MongoDB.Bson;
 using MongoDB.Driver;
-using Musync.Domain.Managers;
 using Musync.Domain.Models;
-using Musync.Domain.Service;
+using Musync.Domain.Services;
 
 namespace musync.api.Repository
 {
     public class TestRepository : BaseMusicRepository, ITestRepository
     {
-        ITestRepository _testRepository;
+        ITestService _testService;
 
-        public TestRepository(ITestRepository testRepository)
+        public TestRepository(ITestService testService)
         {
-            _testRepository = testRepository;
+            _testService = testService;
         }
 
         public DeleteResult DeleteById(ObjectId id)
         {
-            return _testRepository.DeleteById(id);
+            return _testService.DeleteById(id);
         }
 
         public List<TestModel> GetAll()
         {
-            return _testRepository.GetAll().ToList();
+            return _testService.GetAll().ToList();
         }
 
         public TestModel GetById(ObjectId id)
         {
-            return _testRepository.GetById(id);
+            return _testService.GetById(id);
         }
 
         public void Insert(TestModel model)
         {
-            _testRepository.Insert(model);
+            _testService.Insert(model);
         }
 
         public UpdateResult UpdateById(ObjectId id, UpdateDefinition<TestModel> update)
         {
-            var result = _testRepository.UpdateById(id, update);
+            var result = _testService.UpdateById(id, update);
 
             return result;
         }
