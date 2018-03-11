f29b49b2788da165067fc2b5dca25bb3df82ac12
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-11 16:19:08 +0100

    Repositories was created.

---
+++ src/MusyncApi/Repository/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using musync.api.Repository.Interfaces;
+
+namespace musync.api.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+    }
+}
