51f87f6c85577eab4e6fd5699e3b613ef0142ed7
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-11 16:19:31 +0100

    Models was created.

---
+++ src/MusyncApi/Models/AlbumVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace musync.api.Models
+{
+    public class AlbumVM
+    {
+    }
+}
---
+++ src/MusyncApi/Models/UserVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace musync.api.Models
+{
+    public class UserVM
+    {
+        public ObjectId id { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string EmailAdress { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+
+        public string Country { get; set; }
+
+        public string Password { get; set; }
+    }
+}
