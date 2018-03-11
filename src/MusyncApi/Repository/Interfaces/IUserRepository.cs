100a345c9d9685c4df4ebc13a1418fa3aa1ee1f0
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-11 17:50:23 +0100

    IUser Repository was written

--- src/MusyncApi/Repository/Interfaces/IUserRepository.cs
+++ src/MusyncApi/Repository/Interfaces/IUserRepository.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Musync.Domain.Models;
 
 namespace musync.api.Repository.Interfaces
 {
-    interface IUserRepository
+    public interface IUserRepository
     {
+        long DeleteById(ObjectId id);
+
+        IQueryable<User> GetAll();
+
+        User GetById(ObjectId id);
+
+        short Insert(User model);
+
+        long SuperDelete(ObjectId id);
+
+        long ChangePassword(ObjectId id, string oldPAssword, string newPassword);
     }
 }
