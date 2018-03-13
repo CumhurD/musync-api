83070607be759dc701cc57fbcf9e026ff301e582
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-13 15:00:49 +0100

    Setup Method was written

---
+++ src/MusyncApi.Tests/UserControllerTests/When_Super_Delete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using musync.api.Controllers;
+using musync.api.Repository.Interfaces;
+using Moq;
+using NUnit.Framework;
+
+namespace musync.api.tests.UserControllerTests
+{
+    [TestFixture]
+    public class When_Super_Delete
+    {
+        private Mock<IUserRepository> _mockedUserRepository;
+
+        private UserController _userController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockedUserRepository = new Mock<IUserRepository>();
+
+            _userController = new UserController(_mockedUserRepository.Object);
+        }
+    }
+}
