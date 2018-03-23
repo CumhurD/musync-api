2392a8d1e133f1bf7ae9a1447fb5a82100c38357
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-23 15:48:21 +0100

    ISongRepository's access was fixed.

--- src/MusyncApi/Repository/Interfaces/ISongRepository.cs
+++ src/MusyncApi/Repository/Interfaces/ISongRepository.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using Musync.Domain.Models;
 
 namespace musync.api.Repository.Interfaces
 {
-    interface ISongRepository
+    public interface ISongRepository
     {
+        long DeleteById(ObjectId id);
+
+        IQueryable<Song> GetAll();
+
+        Song GetById(ObjectId id);
+
+        short Insert(Song model);
+
+        long SuperDelete(ObjectId id);
     }
 }
