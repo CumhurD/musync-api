ec6088b4c01e624a3f6f988e3adfa867b5335517
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-23 15:48:37 +0100

    SongRepository was written.

--- src/MusyncApi/Repository/SongRepository.cs
+++ src/MusyncApi/Repository/SongRepository.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using musync.api.Repository.Interfaces;
+using MongoDB.Bson;
+using Musync.Domain.Models;
+using Musync.Domain.Services.Interfaces;
 
 namespace musync.api.Repository
 {
     public class SongRepository : ISongRepository
     {
+        ISongService _songService;
+
+        public SongRepository(ISongService songService)
+        {
+            _songService = songService;
+        }
+
+        public long DeleteById(ObjectId id)
+        {
+            return _songService.Delete(id);
+        }
+
+        public IQueryable<Song> GetAll()
+        {
+            return _songService.GetAll();
+        }
+
+        public Song GetById(ObjectId id)
+        {
+            return _songService.GetById(id);
+        }
+
+        public short Insert(Song model)
+        {
+            return _songService.Insert(model);
+        }
+
+        public long SuperDelete(ObjectId id)
+        {
+            return _songService.SuperDelete(id);
+        }
     }
 }
