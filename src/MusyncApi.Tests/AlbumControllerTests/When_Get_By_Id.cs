8bc884e47c7f2627b35dbb6ec474aacb2204e14c
YÜCEL AKTAŞ <yucelaktas91@gmail.com>
2018-03-23 17:07:29 +0100

    Unit Test was written

---
+++ src/MusyncApi.Tests/AlbumControllerTests/When_Get_By_Id.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Http;
+using FluentAssertions;
+using musync.api.Controllers;
+using musync.api.Models;
+using musync.api.Repository.Interfaces;
+using MongoDB.Bson;
+using Moq;
+using Musync.Domain.Models;
+using NUnit.Framework;
+
+namespace musync.api.tests.AlbumControllerTests
+{
+    [TestFixture]
+    public class When_Get_By_Id
+    {
+        private Mock<IAlbumRepository> _mockedAlbumRepository;
+
+        private AlbumController _albumController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockedAlbumRepository = new Mock<IAlbumRepository>();
+
+            _albumController = new AlbumController(_mockedAlbumRepository.Object);
+        }
+
+        [Test]
+        public void Should_Throw_BadRequest_If_There_Is_An_Exception()
+        {
+            ObjectId objectId = new ObjectId();
+
+            _mockedAlbumRepository.Setup(x => x.GetById(It.IsAny<ObjectId>())).Throws(new Exception());
+
+            var exception = Assert.Throws<HttpResponseException>(() => _albumController.Get(objectId));
+
+            exception.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Test]
+        public void Should_Return_AlbumVM_Properly()
+        {
+            ObjectId id = new ObjectId();
+
+            AlbumVM albumVM = new AlbumVM
+            {
+                DisplayName = "test",
+                ArtistsIdentities = new List<ObjectId>() { new ObjectId() },
+                SongIdentities = new List<ObjectId>() { new ObjectId() }
+            };
+
+            Album album = new Album()
+            {
+                DisplayName = "test",
+                ArtistsIdentities = new List<ObjectId>() { new ObjectId() },
+                SongIdentities = new List<ObjectId>() { new ObjectId() }
+            };
+
+            _mockedAlbumRepository.Setup(x => x.GetById(id)).Returns(album);
+
+            var result = _albumController.Get(id);
+
+            result.DisplayName.Should().Be(albumVM.DisplayName);
+
+            result.Id.Should().Be(albumVM.Id);
+
+            result.ArtistsIdentities.ToList()[0].Should().Be(albumVM.ArtistsIdentities.ToList()[0]);
+
+            result.SongIdentities.ToList()[0].Should().Be(albumVM.SongIdentities.ToList()[0]);
+        }
+    }
+}
---
+++ src/MusyncApi.Tests/AlbumControllerTests/When_Super_Delete.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Web.Http;
+using FluentAssertions;
+using musync.api.Controllers;
+using musync.api.Repository.Interfaces;
+using MongoDB.Bson;
+using Moq;
+using NUnit.Framework;
+
+namespace musync.api.tests.AlbumControllerTests
+{
+    [TestFixture]
+    public class When_Super_Delete
+    {
+        private Mock<IAlbumRepository> _mockedAlbumRepository;
+
+        private AlbumController _albumController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockedAlbumRepository = new Mock<IAlbumRepository>();
+
+            _albumController = new AlbumController(_mockedAlbumRepository.Object);
+        }
+
+        [Test]
+        public void Should_Throw_BadRequest_If_There_Is_An_Exception()
+        {
+            ObjectId objectId = new ObjectId();
+
+            _mockedAlbumRepository.Setup(x => x.SuperDelete(It.IsAny<ObjectId>())).Throws(new Exception());
+
+            var exception = Assert.Throws<HttpResponseException>(() => _albumController.SuperDelete(objectId));
+
+            exception.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+
+        [Test]
+        public void Should_Return_One_If_Deleted_Properly()
+        {
+            ObjectId id = new ObjectId();
+
+            _mockedAlbumRepository.Setup(x => x.SuperDelete(id)).Returns(1);
+
+            _albumController.SuperDelete(id).Should().Be(1);
+        }
+    }
+}
